9f649f8c4581e5f37ee121e284fcc2a305a8926b
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-10 18:17:13 +0530

    google font added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+        rel="stylesheet"
+      />
       <body>{children}</body>
     </html>
   );
