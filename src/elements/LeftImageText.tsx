3fdb27cc771180b283f8a4ae61d8a8e544ab0db0
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-11 22:34:01 +0530

    responsive error solved

--- src/elements/LeftImageText.tsx
+++ src/elements/LeftImageText.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 function LeftImageText() {
   return (
-    <div className="flex flex-col md:flex-row w-screen h-fit items-center md:px-[20%] p-[2%] bg-[#0B0319]">
-      <div className="flex w-screen md:w-[40%] h-fit py-10 md:py-0 justify-center items-center text-white">
+    <div className="flex flex-col md:flex-row w-screen h-fit items-center md:px-[20%] p-[4%] bg-[#0B0319]">
+      <div className="md:flex w-screen md:w-[40%] h-fit py-10 md:py-0 justify-center items-center text-white hidden">
         <img src="/three-img.webp" alt="meta verse" />
       </div>
       <Description
@@ -14,6 +14,9 @@ function LeftImageText() {
           para: "The Future of Virtual Reality: Where Pixels Dream. In the not-so-distant future, pixels will transcend screens. They’ll escape their two-dimensional confines and leap into our reality, dancing with our senses.",
         }}
       />
+      <div className="flex w-screen md:w-[40%] h-fit py-10 md:py-0 justify-center items-center text-white md:hidden">
+        <img src="/three-img.webp" alt="meta verse" />
+      </div>
     </div>
   );
 }
