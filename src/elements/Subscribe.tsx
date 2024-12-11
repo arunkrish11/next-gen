46943d1a5e1f0e45d490001e1d24940adbadd749
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-11 13:51:35 +0530

    button size

--- src/elements/Subscribe.tsx
+++ src/elements/Subscribe.tsx
@@ -3,8 +3,8 @@ import React from "react";
 function Subscribe() {
   return (
     <div className="flex flex-col w-screen h-fit py-20 justify-center items-center text-white bg-[#0B0319] ">
-      <div className="w-[50%] h-fit rounded-3xl px-1 py-1 josefin-sans bg-gradient-to-b from-[#DF00BC] to-[#2AA9DF]">
-        <div className="rounded-3xl px-24 py-5 bg-[#0B0319] ">
+      <div className="w-[80%] md:w-[50%] h-fit rounded-3xl px-1 py-1 josefin-sans bg-gradient-to-b from-[#DF00BC] to-[#2AA9DF]">
+        <div className="rounded-3xl px-5 md:px-24 py-5 bg-[#0B0319] ">
           <div className="flex justify-center py-2">
             <h1 className="text-[25px] text-center">
               SUBSCRIBE TO OUR NEWSLETTER
@@ -20,7 +20,7 @@ function Subscribe() {
             </p>
           </div>
           <div className="flex justify-center py-2">
-            <button className="text-[22px] md:px-36 py-1 rounded-3xl text-center bg-gradient-to-t from-[#DF00BC] to-[#2AA9DF] ">
+            <button className="text-[22px] px-[20%] md:px-[30%] py-1 rounded-3xl text-center bg-gradient-to-t from-[#DF00BC] to-[#2AA9DF] ">
               SUBSCRIBE
             </button>
           </div>
