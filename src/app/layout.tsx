020f94964b64e44221561757b62f9e3d36ed67e1
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-11 22:32:30 +0530

    added meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
         href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Poppins&display=swap"
         rel="stylesheet"
       />
-
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body>{children}</body>
     </html>
   );
