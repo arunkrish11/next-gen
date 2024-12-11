cae22bf3da404ad7aa0d7463b3e293a20b702279
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-11 13:12:50 +0530

    font added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,10 @@ export default function RootLayout({
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Poppins&display=swap"
         rel="stylesheet"
       />
+
       <body>{children}</body>
     </html>
   );
