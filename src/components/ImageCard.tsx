230bf065add1cc90822695b736622054a863468f
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-11 13:51:00 +0530

    text align center

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -4,13 +4,9 @@ function ImageCard({ card }: { card: any }) {
   return (
     <div className="flex flex-col w-screen md:w-1/4 h-fit my-6 md:my-0 justify-center items-center">
       <div className="flex md:justify-center items-center ">
-        <img
-          className="w-fit h-fit"
-          src={card.link}
-          alt={card.name}
-        />
+        <img className="w-fit h-fit" src={card.link} alt={card.name} />
       </div>
-      <div className="flex py-6 poppins-regular text-[20px] text-white">
+      <div className="flex py-6 poppins-regular text-center text-[20px] text-white">
         <h1> {card.name} </h1>
       </div>
     </div>
