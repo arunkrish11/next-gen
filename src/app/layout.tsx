4a92166af71d73a5196b20e05861089ec328ff43
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-13 19:04:28 +0530

    head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,13 +7,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Inter:opsz@14..32&family=Josefin+Sans&family=Poppins&display=swap"
-        rel="stylesheet"
-      />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:opsz@14..32&family=Josefin+Sans&family=Poppins&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
