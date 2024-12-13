bb48d90547b0d117dbe59a0e3ddce4432a651af7
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-12-13 11:53:41 +0530

    inter font added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Poppins&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Inter:opsz@14..32&family=Josefin+Sans&family=Poppins&display=swap"
         rel="stylesheet"
       />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
